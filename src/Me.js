b18f77334c50bd5807c3477d49785071ad89c8e5
Asa Tirsen <asa.tirsen@gmail.com>
2020-09-04 11:46:30 +0200

    WebStorm project

--- src/Me.js
+++ src/Me.js
@@ -4,6 +4,9 @@ import "./style/App.scss";
 function Me () {
     return (
         <article className= 'main'>
+            <img src={kids} alt="Bild på mina barn" />
+
+            <div className='textarea'>
         <h1>
             Lite om mig
         </h1>
@@ -12,7 +15,7 @@ function Me () {
 
             <p>Här är en bild på mina barn när de njuter av skogens alla blåbär strax efter att vi återvände till Sverige
             efter ett drygt år i Australien.</p>
-            <img style={{width: '427px', height: '320px'}} src={kids} alt="Bild på mina barn" />
+            </div>
         </article>
     )
 }
