e63a48672e0832c5969f148b747a873ac57d47b8
Asa Tirsen <asa.tirsen@gmail.com>
2020-09-04 08:47:50 +0200

    WebStorm project

--- src/Me.js
+++ src/Me.js
@@ -1,11 +1,20 @@
 import React from "react";
-
-function Home () {
+import kids from "./minaBarn.jpeg";
+import "./style/App.scss";
+function Me () {
     return (
+        <article className= 'main'>
         <h1>
-            HOME
+            Lite om mig
         </h1>
+            <p>Jag heter Åsa och pluggar webbprogrammering på BTH. Jag har just påbörjat mitt andra år och läser just nu
+            kursen js ramverk. Det verkar kul och utmanande.</p>
+
+            <p>Här är en bild på mina barn när de njuter av skogens alla blåbär strax efter att vi återvände till Sverige
+            efter ett drygt år i Australien.</p>
+            <img style={{width: '427px', height: '320px'}} src={kids} alt="Bild på mina barn" />
+        </article>
     )
 }
 
-export default Home;
+export default Me;
